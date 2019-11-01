6b833038ef56ce39f085136b0f779704ef17f386
dawidjk <dawidjk2@illinois.edu>
2019-11-01 18:39:07 -0500

    cleanup

--- frontend/src/app/router/router.component.ts
+++ frontend/src/app/router/router.component.ts
@@ -5,7 +5,6 @@ import { ResultComponent } from '../result/result.component';
 
 const routes: Routes = [
   { path: '', component: SearchbarComponent },
-  { path: '**', redirectTo: 'results' },
   { path: 'search', component: SearchbarComponent },
   { path: 'results', component: ResultComponent },
   { path: 'results/:id', component: ResultComponent }
